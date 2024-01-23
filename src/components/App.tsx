2294a32a3194863207cdec2cfbaff1aa60a83848
Antonov-S <anto1nov1@gmail.com>
2024-01-24 01:52:06 +0200

    Showing Search results and prevent prop drilling (by using children pattern)

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,16 +1,63 @@
+import { useEffect, useState } from "react";
+
 import Background from "./Background";
 import Container from "./Container";
 import Footer from "./Footer";
-import Header from "./Header";
+import Header, { HeaderTop } from "./Header";
+import Logo from "./Logo";
+import BookmarksButton from "./BookmarksButton";
+import SearchForm from "./SearchForm";
+import Sidebar, { SidebarTop } from "./Sidebar";
+import JobItemContent from "./JobItemContent";
+import ResultsCount from "./ResultsCount";
+import SortingControls from "./SortingControls";
+import JobList from "./JobList";
+import PaginationControls from "./PaginationControls";
+
+import { BASE_URL } from "../lib/constants";
 
 function App() {
+  const [searchText, setSearchText] = useState("");
+  const [jobItems, setJobItems] = useState([]);
+
+  useEffect(() => {
+    if (!searchText) return;
+
+    const fetchData = async () => {
+      const response = await fetch(BASE_URL + `?search=${searchText}`);
+      const data = await response.json();
+      setJobItems(data.jobItems);
+    };
+
+    fetchData();
+  }, [searchText]);
+
   return (
     <>
       <Background />
 
-      <Header />
+      <Header>
+        <HeaderTop>
+          <Logo />
+          <BookmarksButton />
+        </HeaderTop>
+        <SearchForm searchText={searchText} setSearchText={setSearchText} />
+      </Header>
 
-      <Container />
+      <Container>
+        <Sidebar>
+          <SidebarTop>
+            <ResultsCount />
+            <SortingControls />
+          </SidebarTop>
+
+          <JobList jobItems={jobItems} />
+
+          <PaginationControls />
+        </Sidebar>
+
+        <JobItemContent />
+      </Container>
 
       <Footer />
     </>
