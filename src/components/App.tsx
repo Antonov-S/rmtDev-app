f615727a177ab1d36cb853763910278fb8d149da
Antonov-S <anto1nov1@gmail.com>
2024-02-01 01:31:39 +0200

    Add basic pagination to the app

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -18,12 +18,24 @@ import PaginationControls from "./PaginationControls";
 import { Toaster } from "react-hot-toast";
 
 function App() {
+  // state
   const [searchText, setSearchText] = useState("");
   const debouncedSearchText = useDebounce(searchText, 350);
   const { jobItems, isLoading } = useJobItems(debouncedSearchText);
+  const [currentPage, setCurrentPage] = useState(1);
 
+  // derived state
   const jobItemsSliced = jobItems?.slice(0, 7) || [];
   const totalNumberOfResults = jobItems?.length || 0;
+
+  // actions
+  const handleChangePage = (direction: "next" | "previous") => {
+    if (direction === "next") {
+      setCurrentPage(prev => prev + 1);
+    } else if (direction === "previous") {
+      setCurrentPage(prev => prev - 1);
+    }
+  };
 
   return (
     <>
@@ -46,7 +58,10 @@ function App() {
 
           <JobList jobItems={jobItemsSliced} isLoading={isLoading} />
 
-          <PaginationControls />
+          <PaginationControls
+            currentPage={currentPage}
+            onClick={handleChangePage}
+          />
         </Sidebar>
 
         <JobItemContent />
