8fb6d5610d6c3fb4ef87208e340a91fa130b79e3
Antonov-S <anto1nov1@gmail.com>
2024-02-05 21:06:20 +0200

    Keep track of bookmarked ids in context API

--- src/components/BookmarkIcon.tsx
+++ src/components/BookmarkIcon.tsx
@@ -1,9 +1,20 @@
+import { useContext } from "react";
 import { BookmarkFilledIcon } from "@radix-ui/react-icons";
 
-export default function BookmarkIcon() {
+import { BookmarksContext } from "../contexts/BookmarksContextProvider";
+
+type BookmarkIconProps = {
+  id: number;
+};
+
+export default function BookmarkIcon({ id }: BookmarkIcon) {
+  const { bookmarkedIds, handleToggleBookmark } = useContext(BookmarksContext);
+
   return (
-    <button className="bookmark-btn">
-      <BookmarkFilledIcon className="filled" />
+    <button onClick={() => handleToggleBookmark(id)} className="bookmark-btn">
+      <BookmarkFilledIcon
+        className={`${bookmarkedIds.includes(id) ? "filled" : ""}`}
+      />
     </button>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import App from "./components/App.tsx";
+import BookmarksContextProvider from "./contexts/BookmarksContextProvider.tsx";
+
 import "./index.css";
 
 const queryClient = new QueryClient();
@@ -10,7 +12,9 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <BookmarksContextProvider>
+        <App />
+      </BookmarksContextProvider>
     </QueryClientProvider>
   </React.StrictMode>
 );
